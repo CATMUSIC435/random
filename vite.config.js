c93d0f1c1d18c094beab579d9a9d985a2ab4ae7c
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-10-18 11:26:48 +0700

    init

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
-import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +12,6 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
-    viteCompression(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
