f5edc784a05a48005a50594b9b226da0693af948
DongIOT <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2025-10-18 11:40:43 +0700

    init

--- src/components/RandomDisplay.jsx
+++ src/components/RandomDisplay.jsx
@@ -9,7 +9,7 @@ export default function RandomDisplay({
       {displayNumber && (
         <div
           ref={numberRef}
-          className="text-[156px] font-extrabold text-white transition-all duration-200 ease-in-out"
+          className="cinzel text-[356px] font-extrabold text-white transition-all duration-200 ease-in-out"
         >
           {displayNumber}
         </div>
